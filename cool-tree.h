8b65700d87f7afc99ce9825df0c2aeb71767fac9
Tadeu Rahian <tadeurahian@gmail.com>
2017-01-16 22:51:14 -0200

    Correcoes

--- cool-tree.h
+++ cool-tree.h
@@ -15,7 +15,41 @@
 
 // define the class for phylum
 // define simple phylum - Program
-typedef class Program_class *Program;
+#define yylineno curr_lineno;
+extern int yylineno;
+
+inline Boolean copy_Boolean(Boolean b) {return b; }
+inline void assert_Boolean(Boolean) {}
+inline void dump_Boolean(ostream& stream, int padding, Boolean b)
+	{ stream << pad(padding) << (int) b << "\n"; }
+
+void dump_Symbol(ostream& stream, int padding, Symbol b);
+void assert_Symbol(Symbol b);
+Symbol copy_Symbol(Symbol b);
+
+class Program_class;
+typedef Program_class *Program;
+class Class__class;
+typedef Class__class *Class_;
+class Feature_class;
+typedef Feature_class *Feature;
+class Formal_class;
+typedef Formal_class *Formal;
+class Expression_class;
+typedef Expression_class *Expression;
+class Case_class;
+typedef Case_class *Case;
+
+typedef list_node<Class_> Classes_class;
+typedef Classes_class *Classes;
+typedef list_node<Feature> Features_class;
+typedef Features_class *Features;
+typedef list_node<Formal> Formals_class;
+typedef Formals_class *Formals;
+typedef list_node<Expression> Expressions_class;
+typedef Expressions_class *Expressions;
+typedef list_node<Case> Cases_class;
+typedef Cases_class *Cases;
 
 class Program_class : public tree_node {
 public:
